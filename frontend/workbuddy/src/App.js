b054cc8baa702cc50b38b39622a49c47f504cea3
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2025-06-28 23:11:02 +0530

    jayakumar first commit

--- frontend/workbuddy/src/App.js
+++ frontend/workbuddy/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Base from "./Components/base"
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Base />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/workbuddy/src/Components/base.jsx
@@ -0,0 +1,12 @@
+const Base=()=>
+{
+    return(
+        <div>
+
+
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Base
