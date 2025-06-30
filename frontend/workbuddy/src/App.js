17e6c6afcbeb51e2f5ee05d38f9290bb72e6af34
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2025-06-30 17:35:41 +0530

    Upto worker request page

--- frontend/workbuddy/src/App.js
+++ frontend/workbuddy/src/App.js
@@ -1,10 +1,11 @@
-// App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import SignUp from "./Components/SignUp";
 import WorkerHome from "./Components/WorkerHomePage/Home";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import PublicRoute from "./Components/PublicRoute";
+import { UserProvider } from "./context/UserContext";
+import WorkerRequests from "./Components/Worker Componets/WorkerRequests";
 
 function App() {
   return (
@@ -23,19 +24,29 @@ function App() {
               }
             />
 
-          {/* Protected Route: Worker Dashboard */}
-          <Route
-            path="/worker/home"
-            element={
-              <ProtectedRoute>
-                <WorkerHome />
-              </ProtectedRoute>
-            }
-          />
+            {/* Protected Route: Worker Dashboard */}
+            <Route
+              path="/worker/home"
+              element={
+                <ProtectedRoute>
+                  <WorkerHome />
+                </ProtectedRoute>
+              }
+            />
 
-        </Routes>
-      </div>
-    </Router>
+            {/* Protected Route: Worker Requests */}
+            <Route
+              path="/worker/request"
+              element={
+                <ProtectedRoute>
+                  <WorkerRequests />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
